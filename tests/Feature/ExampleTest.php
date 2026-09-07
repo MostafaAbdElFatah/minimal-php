c11bce960672b69782cacf03275e7087d30971b6
m.abdelfattah <m.abdelfatah.fci@gmail.com>
2026-09-07 09:15:24 +0300

    test: cover guest and authenticated home access
    Add authentication coverage for the home page and document test enforcement
    guidelines for the project.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,16 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('guests are redirected to login', function () {
+    /** @var \Tests\TestCase $this */
     $response = $this->get('/');
+    $response->assertRedirect('/login');
+});
+
+test('authenticated users can view the home page', function () {
+    /** @var \Tests\TestCase $this */
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this->actingAs($user)->get('/');
 
     $response->assertStatus(200);
 });
