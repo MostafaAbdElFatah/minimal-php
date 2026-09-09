f7c8466cabab7cdb0f0cc4e4e5bb2d3ab5c97bd9
m.abdelfattah <m.abdelfatah.fci@gmail.com>
2026-09-09 08:03:40 +0300

    .

--- database/seeders/IdeaSeeder.php
+++ database/seeders/IdeaSeeder.php
@@ -13,9 +13,35 @@ class IdeaSeeder extends Seeder
      */
     public function run(): void
     {
-        Idea::factory()->count(100)->create([
-            'user_id' => User::inRandomOrder()->value('id'),
-        ]);
         Idea::factory()->count(200)->create(['user_id' => 2]);
+
+        //fetch all ids
+        //$userIds = User::pluck('id')->toArray();
+
+        //fetch all ids expect 1
+        $userIds = User::where('id', '!=', 1)
+            ->pluck('id')
+            ->toArray();
+
+        //fetch all ids expect 1, 2, 3
+        // $userIds = User::whereNotIn('id', [1, 2, 3])
+        //     ->pluck('id')
+        //     ->toArray();
+
+        ///random user_Id in each record in 600 record
+        Idea::factory()
+            ->count(600)
+            ->state(fn() => [
+                'user_id' => fake()->randomElement($userIds),
+            ])
+            ->create();
+
+        //same user_Id in all 600 record
+        // Idea::factory()->count(600)->create([
+        //     'user_id' => fake()->randomElement($userIds),
+        // ]);
+        // Idea::factory()->count(100)->create([
+        //     'user_id' => User::inRandomOrder()->value('id'),
+        // ]);
     }
 }
