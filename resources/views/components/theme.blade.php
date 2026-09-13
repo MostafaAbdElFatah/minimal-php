8b55095e897bec9e697c8c995b186ebd5f69ed0e
m.abdelfattah <m.abdelfatah.fci@gmail.com>
2026-09-13 10:24:37 +0300

    refactor: extract password interactions into dedicated module - move password toggle and strength logic out of the layout - include the new module in the main JavaScript entrypoint - simplify theme menu markup - remove unused asd.html

--- resources/views/components/theme.blade.php
+++ resources/views/components/theme.blade.php
@@ -1,10 +1,10 @@
 <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
         <li>
-            <details class="dropdown dropdown-end" data-menu="theme">
+            <details data-menu="theme">
                 <summary>Theme</summary>
 
-                <ul class="menu menu-sm dropdown-content z-50 max-h-96 w-52 overflow-y-auto rounded-box bg-base-100 p-2 shadow">
+                <ul class="z-50 max-h-96 w-52 overflow-y-auto rounded-t-none bg-base-100 p-2 shadow">
                     <li><input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start" aria-label="Light" value="light" /></li>
                     <li><input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start" aria-label="Dark" value="dark" /></li>
                     <li><input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start" aria-label="Cupcake" value="cupcake" /></li>
